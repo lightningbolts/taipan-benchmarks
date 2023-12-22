5a0ad8950a3f5a4b3a0eb5335fb880e4aecd0c29
lightningbolts <timberlake2025@gmail.com>
2023-12-21 19:58:20 -0800

    First commit

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Feed from "@components/Feed"
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Taipan Benchmarks
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">Discover all sorts of easy to use benchmarks.</span>
+      </h1>
+      <p className="desc text-center">
+        Taipan Benchmarks is a collection of benchmarks that are easy to use and understand.
+      </p>
+      <Feed />
+
+    </section>
+  )
+}
+
+export default Home

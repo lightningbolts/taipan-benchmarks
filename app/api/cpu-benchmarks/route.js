be96cb0e3dc38cb660d2a59ab686fec8539fe357
lightningbolts <timberlake2025@gmail.com>
2023-12-23 19:45:55 -0800

    Update database connection and API endpoints

--- app/api/cpu-benchmarks/route.js
+++ app/api/cpu-benchmarks/route.js
@@ -1,12 +1,26 @@
-import { connectToTaipanDatabase } from "@utils/database";
-import Prompt from "@models/prompt";
-
+import { connectToDatabase } from "@utils/database";
+import Bench from "@models/benchmark";
 export const GET = async (request) => {
     try {
-        await connectToTaipanDatabase();
-        const prompts = await Prompt.find({}).populate("creator");
-        return new Response(JSON.stringify(prompts), { status: 200 });
+        await connectToDatabase();
+        const benchmarks = await Bench.find({});
+        return new Response(JSON.stringify(benchmarks), { status: 200 });
     } catch (error) {
         return new Response("Failed to fetch all cpu benchmarks", { status: 500 });
+    }
+}
+
+export const POST = async (request, res) => {
+    // const { cpu_model, os_info, digits, single_core_score, multi_core_score, speedup, efficiency, cpu_utilization, time, hostname } = await request.json();
+    const bench = await request.json();
+
+    try {
+        await connectToDatabase();
+        const newBench = await Bench.create(bench);
+        await newBench.save();
+        return new Response(JSON.stringify(newBench), { status: 201 });
+
+    } catch (error) {
+        return new Response("Failed to create a new cpu benchmark", { status: 500 });
     }
 }
---
+++ models/benchmark.js
@@ -0,0 +1,48 @@
+import { Schema, model, models } from "mongoose";
+
+const BenchSchema = new Schema({
+    cpu_model: {
+        type: String,
+        required: [true, "CPU model is required"],
+    },
+    os_info: {
+        type: String,
+        required: [true, "OS Info is required"],
+    },
+    digits: {
+        type: Number,
+        required: [true, "Digits is required"],
+    },
+    single_core_score: {
+        type: Number,
+        required: [true, "Single core score is required"],
+    },
+    multi_core_score: {
+        type: Number,
+        required: [true, "Multi core score is required"],
+    },
+    speedup: {
+        type: Number,
+        required: [true, "Speedup is required"],
+    },
+    efficiency: {
+        type: Number,
+        required: [true, "Efficiency is required"],
+    },
+    cpu_utilization: {
+        type: Number,
+        required: [true, "CPU utilization is required"],
+    },
+    time: {
+        type: String,
+        required: [true, "Time is required"],
+    },
+    hostname: {
+        type: String,
+        required: [true, "Host name is required"],
+    },
+});
+
+const Bench = models.Bench || model("Bench", BenchSchema);
+
+export default Bench;

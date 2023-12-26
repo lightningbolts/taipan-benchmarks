481f55df7eb8a3bb9870fe19adc1757fb330cd1c
lightningbolts <timberlake2025@gmail.com>
2023-12-26 10:04:29 -0800

    Add user login status and display appropriate message

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,29 @@
+"use client"
 import Feed from "@components/Feed"
 import BenchmarkFeed from "@components/BenchmarkFeed"
+import { useSession } from "next-auth/react"
 
 const Home = () => {
+  // Check if user is logged in
+  const { data: session } = useSession();
+
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">Taipan Benchmarks
+        {/* If user is not logged in */}
+        {!session && (
+          <>
+            <br className="max-md:hidden" />
+            <span className="blue_gradient text-center"> Sign in right now.</span>
+          </>
+        )}
+        {/* If user is logged in */}
+        {session && (
+          <>
+            <br className="max-md:hidden" />
+            <span className="blue_gradient text-center"> Start benching.</span>
+          </>
+        )}
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> Discover.</span>
       </h1>
--- app/view-prompts/page.jsx
+++ app/view-prompts/page.jsx
@@ -5,6 +5,8 @@ const PromptFeed = () => {
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">Taipan Benchmarks
                 <br className="max-md:hidden" />
+                <span className="blue_gradient text-center"> Share your post.</span>
+                <br className="max-md:hidden" />
                 <span className="orange_gradient text-center"> Discover.</span>
             </h1>
             <p className="desc text-center">

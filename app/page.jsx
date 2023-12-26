020978426a08f38933692ff52ae87330866df8e1
lightningbolts <timberlake2025@gmail.com>
2023-12-26 10:11:39 -0800

    Add MainText component to Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,32 +1,14 @@
-"use client"
 import Feed from "@components/Feed"
 import BenchmarkFeed from "@components/BenchmarkFeed"
 import { useSession } from "next-auth/react"
+import MainText from "@components/MainText"
 
 const Home = () => {
   // Check if user is logged in
-  const { data: session } = useSession();
 
   return (
     <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">Taipan Benchmarks
-        {/* If user is not logged in */}
-        {!session && (
-          <>
-            <br className="max-md:hidden" />
-            <span className="blue_gradient text-center"> Sign in right now.</span>
-          </>
-        )}
-        {/* If user is logged in */}
-        {session && (
-          <>
-            <br className="max-md:hidden" />
-            <span className="blue_gradient text-center"> Start benching.</span>
-          </>
-        )}
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> Discover.</span>
-      </h1>
+      <MainText />
       <p className="desc text-center">
         Taipan Benchmarks is a collection of benchmarks that are easy to use and understand.
       </p>
---
+++ components/MainText.jsx
@@ -0,0 +1,30 @@
+"use client"
+import { useSession } from 'next-auth/react'
+
+const MainText = () => {
+    const { data: session } = useSession();
+    return (
+        <h1 className="head_text text-center">Taipan Benchmarks
+            {/* If user is not logged in */}
+            {!session && (
+                <>
+                    <br className="max-md:hidden" />
+                    <span className="blue_gradient text-center"> Sign in right now.</span>
+                </>
+            )}
+            {/* If user is logged in */}
+            {session && (
+                <>
+                    <br className="max-md:hidden" />
+                    <span className="blue_gradient text-center"> Start benching.</span>
+                </>
+            )}
+            <>
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center"> Discover.</span>
+            </>
+        </h1>
+    )
+}
+
+export default MainText

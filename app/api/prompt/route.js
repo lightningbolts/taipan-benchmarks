a0be067a52e3b36fcb93b5aff1f392537a7a2031
lightningbolts <timberlake2025@gmail.com>
2023-12-30 19:02:31 -0800

    Add revalidation to GET route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,10 +1,12 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
+import { revalidatePath } from "next/cache";
 
 export const GET = async (request) => {
     try {
         await connectToDatabase();
         const prompts = await Prompt.find({}).populate("creator");
+        revalidatePath(request.url)
         return new Response(JSON.stringify(prompts), { status: 200 });
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 });

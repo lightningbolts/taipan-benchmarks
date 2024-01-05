0195d431d58a87fb3a75fcb29eead87406295362
lightningbolts <timberlake2025@gmail.com>
2024-01-04 16:54:44 -0800

    Uncommented footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ const RootLayout = ({ children }) => {
                         {children}
                         <Analytics />
                     </main>
-                    {/* <Footer /> */}
+                     <Footer />
                 </Provider>
             </body>
         </html>

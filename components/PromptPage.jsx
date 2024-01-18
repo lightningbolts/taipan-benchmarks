185f158cff0a501bb9ebb3041d8ab3ed1d3a089b
lightningbolts <timberlake2025@gmail.com>
2024-01-17 20:27:55 -0800

    Styled separate post page to look nicer

--- components/PromptPage.jsx
+++ components/PromptPage.jsx
@@ -1,35 +1,120 @@
 "use client"
 import { useState } from "react"
 import { useSession } from "next-auth/react";
+import PromptCard from "@components/PromptCard";
+import Image from "@node_modules/next/image";
+import {usePathname, useRouter} from "next/navigation";
+import {set} from "mongoose";
 
 const PromptPage = ({ promptData }) => {
     const { data: session } = useSession();
     console.log(promptData)
+    const [copied, setCopied] = useState("");
+    const pathName = usePathname();
+    const router = useRouter();
+    const [pos, setPos] = useState(null);
+
+
+    const handleEdit = (promptData) => {
+        router.push(`/update-prompt?id=${promptData._id}`)
+    }
+
+    const handleProfileClick = () => {
+        console.log(promptData);
+
+        if (promptData.creator._id === session?.user.id) return router.push("/profile");
+
+        router.push(`/profile/${promptData.creator._id}?name=${promptData.creator.username}`);
+    };
+
+    const handleCopy = () => {
+        setCopied(promptData.prompt);
+        navigator.clipboard.writeText(promptData.prompt);
+        setTimeout(() => setCopied(false), 3000);
+    };
+
+    const handleDelete = async (promptData) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+        if (hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${promptData._id.toString()}`, {
+                    method: "DELETE",
+                })
+
+                setPos(promptData)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    }
+
+    const handleTagClick = (tag) => {
+        router.push(`/tag/${tag}`)
+    }
 
     return (
-        <div className="prompt_page">
-            <div className="prompt_page_container">
-                <div className="prompt_page_header">
-                    <h1 className="prompt_page_title">{promptData.prompt}</h1>
-                    <div className="prompt_page_tags">
-                        {promptData.tag}
+        <div className='prompt_card'>
+            <div className='flex justify-between items-start gap-5'>
+                <div
+                    className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+                    onClick={handleProfileClick}
+                >
+                    <Image
+                        src={promptData.creator.image}
+                        alt='user_image'
+                        width={40}
+                        height={40}
+                        className='rounded-full object-contain'
+                    />
+
+                    <div className='flex flex-col'>
+                        <h3 className='font-satoshi font-semibold text-gray-900'>
+                            {promptData.creator.username}
+                        </h3>
+                        <p className='font-inter text-sm text-gray-500'>
+                            {promptData.creator.email}
+                        </p>
                     </div>
                 </div>
-                <div className="prompt_page_content">
-                    <div className="prompt_page_user">
-                        <div className="prompt_page_user_image">
-                            <img src={promptData.creator.image} alt="" />
-                        </div>
-                        <div className="prompt_page_user_info">
-                            <h3 className="prompt_page_user_name">{promptData.creator.username}</h3>
-                            <p className="prompt_page_user_email">{promptData.creator.email}</p>
-                        </div>
-                    </div>
-                    <div className="prompt_page_copy">
-                        <button className="prompt_page_copy_btn">Copy</button>
-                    </div>
+
+                <div className='copy_btn' onClick={handleCopy}>
+                    <Image
+                        src={
+                            copied === promptData.prompt
+                                ? "/assets/icons/tick.svg"
+                                : "/assets/icons/copy.svg"
+                        }
+                        alt={copied === promptData.prompt ? "tick_icon" : "copy_icon"}
+                        width={12}
+                        height={12}
+                    />
                 </div>
             </div>
+
+            <p className='my-4 font-satoshi text-sm text-gray-700'>{promptData.prompt}</p>
+            <p
+                className='font-inter text-sm blue_gradient cursor-pointer'
+                // onClick={() => handleTagClick && handleTagClick(promptData.tag)}
+            >
+                #{promptData.tag}
+            </p>
+
+            {session?.user.id === promptData.creator._id && pathName === "/profile" && (
+                <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+                    <p
+                        className='font-inter text-sm green_gradient cursor-pointer'
+                        onClick={handleEdit}
+                    >
+                        Edit
+                    </p>
+                    <p
+                        className='font-inter text-sm orange_gradient cursor-pointer'
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </p>
+                </div>
+            )}
         </div>
     )
 }

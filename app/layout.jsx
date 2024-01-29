61b04c3ce276fbf4851c6d186fde8f1febde6d73
lightningbolts <timberlake2025@gmail.com>
2024-01-29 11:17:04 -0800

    Added speedinsights

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,6 +24,7 @@ const RootLayout = ({ children }) => {
                         <Nav />
                         {children}
                         <Analytics />
+                        <SpeedInsights />
                     </main>
 
                 </Provider>

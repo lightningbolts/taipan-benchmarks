9a0a41d641d8c137c97f3c3deb9339e7fb0f43e2
lightningbolts <timberlake2025@gmail.com>
2024-01-29 18:13:34 -0800

    Added source code

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,8 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
       <MainText />
       <p className="desc text-center">
-        Taipan Benchmarks is a collection of benchmarks that are easy to use and understand.
+        Taipan Benchmarks is a collection of benchmarks that are easy to use and understand. Source code is available on
+          <a href="https://github.com/lightningbolts/taipan-benchmarks" className="text-blue-500 hover-brighten"> Github</a>.
       </p>
       {/* <Feed /> */}
       <BenchmarkFeed />
